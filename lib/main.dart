5ccc1e6612f82fb4d548046a1e4a7c4af356712b
mamemaru00 <k.shouhei0711@gmail.com>
2024-02-05 22:41:19 +0900

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   final img = Image.asset('images/banana_kawa_muke.png');
 
-  // ロー
-  final row = Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [img, img, img],
+  // 小さいコンテナ
+  final smallCon = Container(
+    color: Colors.deepOrange, // オレンジ色
+    width: 50,
+    height: 50,
   );
 
+  // 縦に並べるカラム
+  final col = Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      smallCon, // 小さいコンテナ
+      img, // 画像
+      smallCon, // 小さいコンテナ
+    ],
+  );
+
+  // 大きいコンテナ
+  final bigCon = Container(
+    color: Colors.blue, // 青色
+    width: 400,
+    height: 400,
+    alignment: Alignment.centerLeft,
+    padding: const EdgeInsets.all(20),
+    child: col, // 中にカラムを丸ごと入れる
+  );
+
+  // アプリ
   final a = MaterialApp(
-    // `const`を削除
     home: Scaffold(
       body: Center(
-        child: row,
+        child: bigCon, // 大きいコンテナを表示する
       ),
     ),
   );
