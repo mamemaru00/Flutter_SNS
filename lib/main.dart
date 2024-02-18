bbe1658aaba0fbe606b2d25f8d193662f719badd
mamemaru00 <k.shouhei0711@gmail.com>
2024-02-18 22:24:55 +0900

    TextFilde

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,69 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: sized_box_for_whitespace
 
 /*
   最新のFlutterに対応するため、動画と少しコードが変わりました
+  コントローラーの正しい使い方は、別の動画で説明します
+
+  ↓↓ 別の動画 ↓↓
+  https://youtu.be/l1sUCFtcsjo?si=tT8Swp7GuzmbQ6lv
 */
 
+import 'package:flutter/material.dart';
+
 void main() {
-  // ボタンを押したら呼ばれる関数
+  // テキストフィールド の コントローラー
+  final controller = TextEditingController();
+
+  // テキストフィールド本体
+  final textField = TextField(
+    // コントローラ を このテキストフィールドに結びつける
+    controller: controller,
+    // デコレーション
+    decoration: const InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: "あなたの名前",
+      hintText: "カタカナで入力してください",
+      errorText: null, // エラーメッセージは今回使わない。ここに書いたり消したりできる。
+    ),
+  );
+
+  // 関数
   xxxx() {
-    debugPrint('これから通信を始めます');
-    debugPrint('通信中です');
-    debugPrint('通信が終わりました');
+    // コントローラーから文字を取り出して確認
+    debugPrint(controller.text);
   }
 
-  // ボタン本体
+  // ボタン
   final button = ElevatedButton(
+    // 関数を このボタンに結びつけておく
     onPressed: xxxx,
-    style: ElevatedButton.styleFrom(
-      // MEMO: primary は古くなったので backgroundColor へ変更しました
-      backgroundColor: Colors.green,
-    ),
-    child: const Text('押してみて'),
+    child: const Text('ボタンです'),
   );
 
   // アプリ
-  final a = MaterialApp(
+  final app = MaterialApp(
+    // 画面
     home: Scaffold(
+      // 真ん中
       body: Center(
-        child: button,
+        // 縦に並べる
+        child: Column(
+          // いい感じにスペース開ける
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // コンテナ
+            Container(
+              width: 300, // 横幅
+              child: textField, // テキストフィールド
+            ),
+            // ボタンをおく
+            button,
+          ],
+        ),
       ),
     ),
   );
 
-  runApp(a);
+  // アプリを動かす
+  runApp(app);
 }
