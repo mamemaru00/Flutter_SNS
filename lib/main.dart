aa837da8efff148b60af33e850558ffa3550f4ff
mamemaru00 <k.shouhei0711@gmail.com>
2024-02-18 22:01:31 +0900

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_create_new/banana_counter.dart';
+
+/*
+  最新のFlutterに対応するため、動画と少しコードが変わりました
+*/
 
 void main() {
-  // バナナカウンターを使う
-  const bnn = BananaCounter(
-    number: 888,
+  // ボタンを押したら呼ばれる関数
+  xxxx() {
+    debugPrint('これから通信を始めます');
+    debugPrint('通信中です');
+    debugPrint('通信が終わりました');
+  }
+
+  // ボタン本体
+  final button = ElevatedButton(
+    onPressed: xxxx,
+    style: ElevatedButton.styleFrom(
+      // MEMO: primary は古くなったので backgroundColor へ変更しました
+      backgroundColor: Colors.green,
+    ),
+    child: const Text('押してみて'),
   );
 
   // アプリ
-  const a = MaterialApp(
+  final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: bnn,
+        child: button,
       ),
     ),
   );
+
   runApp(a);
 }
